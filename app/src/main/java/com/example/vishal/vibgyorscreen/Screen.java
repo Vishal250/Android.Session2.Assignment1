89689d0061f2bf93280bcaffaac923ece96d6830
Vishal250 <vishalnirmal250@gmail.com>
2017-02-15 22:57:18 +0530

    assignment1
    session2

---
+++ app/src/main/java/com/example/vishal/vibgyorscreen/Screen.java
@@ -0,0 +1,13 @@
+package com.example.vishal.vibgyorscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen);
+    }
+}
